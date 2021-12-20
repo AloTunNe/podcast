2ef91d75eaf895251541b85f48b4fa9bd44857e7
VuLoan <84124596+VuLoan@users.noreply.github.com>
2021-12-21 02:10:26 +0700

    update again

---
+++ app/src/main/java/com/example/podcast/Activity/Author.java
@@ -0,0 +1,15 @@
+package com.example.podcast.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.podcast.R;
+
+public class Author extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.author);
+    }
+}
